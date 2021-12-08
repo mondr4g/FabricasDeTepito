525dcb827bcac81f81674ff297eab2702b48c22d
Eduardo Mena <46607259+mondr4g@users.noreply.github.com>
2021-12-08 15:19:49 -0600

    borrar archivos

--- global/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define("KEY","prueba_carro");
-    define("COD","AES-128-ECB");
-    define("SERVER","localhost");
-    define("USUARIO","root");
-    define("PASSWORD","");
-    define("BD","_store_");
-?>
